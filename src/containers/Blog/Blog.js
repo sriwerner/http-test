92c912a93e7deaadc5c915ee1b74d027f9272a70
arch-mac <manuel@sahores.com>
2018-11-17 17:47:33 -0300

    adding interceptors to execute code globally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,76 +1,71 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
-import axios from 'axios';
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
+import axios from "axios";
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: null
-    }
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: null
+  };
 
-    axiosConfig = {
-        headers: {
-            // 'Content-Type': 'application/json;charset=UTF-8',
-            "Access-Control-Allow-Origin": "http://localhost:3000",
-        }
-    }
+  componentDidMount() {
+    // axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+    // axios.defaults.headers.get["Access-Control-Allow-Origin"] = "*";
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Max"
+          };
+        });
 
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts', this.axiosConfig)
-            .then(response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post, 
-                        author: 'Max'
-                    }
-                })
+        this.setState({ posts: updatedPosts });
+        // console.log(posts);
+      })
+      .catch(error => {
+        this.setState({ error: true });
+      });
+  }
 
-                this.setState({posts: updatedPosts})
-                // console.log(posts);
-                
-            })
-            .catch(error => {
-                this.setState({error: true})
-            })
-    }
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
 
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id})
-    }
-    
-    render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
-        if (!this.state.error){
-            posts = this.state.posts.map(
-                post => { return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)}
-                />; 
-            });
-        }
-
+  render() {
+    let posts = <p style={{ textAlign: "center" }}>Something went wrong!</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
         );
+      });
     }
+
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
